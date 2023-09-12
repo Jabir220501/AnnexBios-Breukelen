067cfcb5a37c3eab92ab27e767d6518bf23b35aa
Jabir <220501@student.glu.nl>
2023-09-13 00:21:41 +0200

    Header af, begin aan hero

--- components/Header/header.php
+++ components/Header/header.php
@@ -1,3 +1,26 @@
 <header>
-    
+    <div class="logo">
+        <img src="./assets/images/logo_hoofd.png" alt="">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Film agenda</a></li>
+            <li><a href="#">Alle vestiging</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <div class="hamburg_menu__closed">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+    </div>
 </header>
+<div class="snel_ticekts_kopen__container">
+    <form action="#">
+        <label for="#">Koop je tickets</label>
+        <select name="#" id="#">
+            <option>Kies je film</option>
+        </select>
+        <input type="submit" value="Bestel tickets">
+    </form>
+</div>
