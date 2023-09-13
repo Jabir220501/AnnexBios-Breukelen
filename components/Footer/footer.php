850fdc67d42391084cf6ff5411da6fd9b713c89c
Jabir <220501@student.glu.nl>
2023-09-13 15:29:07 +0200

    footer in progress

--- components/Footer/footer.php
+++ components/Footer/footer.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-    <footer>
-        <div id="container"> 
-            <div class="logo">
-                <img src="./img/logo_hoofd.png">
-            </div>
-            <br>
-            <br>
-            <div class="informatie">sum komt uit de secties 1.10.32 en 1.10.33 van<br> "de Finibus Bonorum et Malorum" (De uitersd<br>heuakhfiosdhfhicehjsdijdfioejwhiudsfhiuchesu<br>sjkdfhisedkjfcjkesdnkjdnfklesajdkljkfdnwkj;ed
+<footer>
+    <div id="container">
+        <div class="logo">
+            <img src="./img/logo_hoofd.png">
+        </div>
+        <br>
+        <br>
+        <div class="informatie">sum komt uit de secties 1.10.32 en 1.10.33 van<br> "de Finibus Bonorum et Malorum" (De
+            uitersd<br>heuakhfiosdhfhicehjsdijdfioejwhiudsfhiuchesu<br>sjkdfhisedkjfcjkesdnkjdnfklesajdkljkfdnwkj;ed
             <br>
             <br>
             <br>
@@ -25,26 +15,22 @@
             </div>
         </div>
         <div id="container-1">
-        <div class="tekst">
-            <h1>NAVIGEER</h1>
-           <div class="navigeerlinks">Werken Bij</div>
-           <div class="navigeerlinks">Veelgestelde Vragen</div>
-           <div class="navigeerlinks">Vestigingen</div>
-           <div class="navigeerlinks">Contact</div>
-        </div>
-        <div id="container-2">
-        <div class="tekst1">
-            <h1>VOLG ONS</h1>
-        </div>
-        <div class="img">
-        <img src="./img/social_media.svg">
-        </div>
-        <div class="voorwaarden">
-            <h3>Voorwaarden | Privacy Beleid | Cookie Disclaimer
-        </div>
-</div>
-        
-        
-           
+            <div class="tekst">
+                <h1>NAVIGEER</h1>
+                <div class="navigeerlinks">Werken Bij</div>
+                <div class="navigeerlinks">Veelgestelde Vragen</div>
+                <div class="navigeerlinks">Vestigingen</div>
+                <div class="navigeerlinks">Contact</div>
+            </div>
+            <div id="container-2">
+                <div class="tekst1">
+                    <h1>VOLG ONS</h1>
+                </div>
+                <div class="img">
+                    <img src="./img/social_media.svg">
+                </div>
+                <div class="voorwaarden">
+                    <h3>Voorwaarden | Privacy Beleid | Cookie Disclaimer
+                </div>
+            </div>
 </footer>
-</html>
