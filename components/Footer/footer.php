67ab974db7154dbceeaa6f482b2717c54d530862
Adil Chikhi <220631@student.glu.nl>
2023-09-13 10:20:39 +0200

    footer

--- components/Footer/footer.php
+++ components/Footer/footer.php
@@ -1,3 +1,43 @@
 <footer>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+    <footer>
+        <div id="container"> 
+            <div class="logo">
+                <img src="./img/logo_hoofd.png">
+            </div>
+            <br>
+            <br>
+            <div class="informatie">sum komt uit de secties 1.10.32 en 1.10.33 van<br> "de Finibus Bonorum et Malorum" (De uitersd<br>heuakhfiosdhfhicehjsdijdfioejwhiudsfhiuchesu<br>sjkdfhisedkjfcjkesdnkjdnfklesajdkljkfdnwkj;ed
+            <br>
+            <br>
+            <br>
+            <div class="lees meer button">
+                <button type="button">Lees Meer</button>
+            </div>
+        </div>
+        <div id="container-1">
+        <div class="tekst">
+            <h1>Navigeer</h1>
+           <div>Werken Bij</div>
+           <div>Veelgestelde Vragen</div>
+           <div>Vestigingen</div>
+           <div>Contact</div>
+        </div>
+        </div>
+        
+           
+</footer>
+</html>
+
 
 </footer>
