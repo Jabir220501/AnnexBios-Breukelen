7b874b70f57788d32e23b1e127e4f3ebd326c082
Adil Chikhi <220631@student.glu.nl>
2023-09-20 15:23:49 +0200

    begin aan film pagina

--- film.php
+++ film.php
@@ -18,13 +18,35 @@
     <main>
         <!-- Film -->
         <section id="film">
+        <section id="film_agenda_xl">
+            <div id="agenda-container">
+                <div class="agenda-text">
+                    <h1>JURASSIC WORLD: FALLEN KINGDOM </h1>
+                </div>
+                
+      
+            
+            <div class="dinofoto">
+                <img src="./assets/images/icons/dino.png " style="width: 680px; height:1014px; margin-top: 20%">
+            </div>
+            <div id="informatie-container">
+                <div class="informatie-tekst">
+                
+                </div>
+                
+            </div>
+            <div class="button">
+                <button type="button" style="width: 800px; margin-top:4%; height: 60px; background-color:#8B9E51; color:white; font-size:20px; ">KOOP JE TICKETS</button>
+                </div>
+        </section>
 
         </section>
+        
     </main>
     <!-- End Main -->
     <!-- Footer -->
     <?php
-    include './components/Footer/footer.php'
+    // include './components/Footer/footer.php'
         ?>
 </body>
 
